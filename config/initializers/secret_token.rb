f3f3a4951f161667d909911715aa39e786c1b9a5
Gonzalez Maxi <emaxi.gonzalez@gmail.com>
2013-02-02 21:34:47 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exching::Application.config.secret_token = '4c699144a25c736668b41c469f18a799b6d04826f82890ab62a7f15f8e36291bf3164f3fea11984bfc0da6056a12211b046f7cc2b34ce1e68cc98442987f890e'
