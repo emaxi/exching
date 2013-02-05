b1e3e666d35d4e90c5871a9e5322339999586b68
Gonzalez Maxi <emaxi.gonzalez@gmail.com>
2013-02-05 14:07:28 -0300

    New page goes to mercury editor

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,10 +1,24 @@
 ActiveAdmin.register Page do
+
   index do
     column :title
     column :actions do |page|
       link_to 'View', page_path(page)
       link_to "Edit Page", "/editor/pages/" + page.id.to_s
+      link_to "Edit Page", "/editor/pages/" + ""
     end
   end
+
+  form do |f|
+    f.input :title
+    f.input :meta_content
+    f.buttons
+  end
+
+  action_item :only => :index do
+    link_to "New Page", '/editor/pages/new'
+  end
+
+  actions  :index
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,31 @@
 class PagesController < ApplicationController
+  def create
+    page = Page.new
+    page.title = params[:content][:page_title][:value]
+    page.content = params[:content][:page_content][:value]
+    page.save!
+    render text: ""
+  end
+
+  def new
+    page = Page.new
+  end
+
+
   def show
-    @page = Page.find(params[:id])
+    begin
+      @page = Page.find(params[:id])
+    rescue
+      @page = Page.new title: 'Some Title', content: 'Duis dolore te, feugait nulla facilisi nam liber tempor cum. Ii legunt saepius claritas est etiam processus dynamicus.'
+    end
   end
 
   def update
-    page = Page.find(params[:id])
+    page = begin
+      Page.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      Page.new
+    end
     page.title = params[:content][:page_title][:value]
     page.content = params[:content][:page_content][:value]
     page.save!
