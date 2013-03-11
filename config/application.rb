2078591b277865e1a127fa092b54ef9badbe30b9
Gonzalez Maxi <emaxi.gonzalez@gmail.com>
2013-03-11 00:25:14 -0300

    Fix requiring gems declared on Gemfile

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,10 @@
-require 'activeadmin'
-require 'devise'
-require 'cells'
-require 'mercury-rails'
-require 'paperclip'
-
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Exching
